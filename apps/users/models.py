f493f2805a91ab7ea909b1c9f009c252c681eb8a
pizi_li <807745654@qq.com>
2018-01-31 15:07:09 +0800

    Update models.py

--- apps/users/models.py
+++ apps/users/models.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class UserProfile(AbstractUser):
-    nick_name = models.CharField(max_length=50, verbose_name=u'昵称', default='')
+    nick_name = models.CharField(max_length=50, verbose_name=u'昵称', default=u'')
     birthday = models.DateField(verbose_name=u'生日', null= True, blank=True)
     gender = models.CharField(max_length=6, choices=(('male',u'男'),('female',u'女')), default='female')
     address = models.CharField(max_length=100, default=u'')
